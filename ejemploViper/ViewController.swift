9f8a4e7ae63641b4f68fdec0d98c70aea8a2684a
Salvador Lopez Camacho <salvatore.isc@gmail.com>
2020-09-11 08:47:39 -0500

    Initial Commit

---
+++ ejemploViper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemploViper
+//
+//  Created by Salvador Lopez on 11/09/20.
+//  Copyright © 2020 Salvador Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
